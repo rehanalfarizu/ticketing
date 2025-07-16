ff81eda6a5ac6ac8766306ea16034fed476ced14
rehanalfarizu <rehanalfarizu>
2025-07-17 02:10:02 +0700

    laravel+vue ticket2

---
+++ frontend/src/plugins/axios.js
@@ -0,0 +1,25 @@
+// frontend/src/plugins/axios.js
+import axios from 'axios'
+
+const api = axios.create({
+  baseURL: 'http://localhost:8000/api',
+  withCredentials: true,
+  headers: {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json'
+  }
+})
+
+// Request interceptor
+api.interceptors.request.use(
+  config => {
+    const token = localStorage.getItem('auth_token')
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`
+    }
+    return config
+  },
+  error => Promise.reject(error)
+)
+
+export default api
